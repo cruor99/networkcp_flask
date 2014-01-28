3192f86a2a30de2f6a9b2d3f024536ef5de0a35f
KjetilAndre <kliknes@gmail.com>
2014-01-28 17:48:32 +0100

    more login stuff

--- networkcp_flask.py
+++ networkcp_flask.py
@@ -5,15 +5,16 @@ import deployserv
 app = Flask(__name__)
 app.secret_key='minecraft'
 
-def login_required(test):
-    @wraps(test)
-    def wrap(*args, **kwargs):
-        if 'logged_in' in session:
-            return test(*args, **kwargs)
-        else:
-            flash('You need to log in first.')
-            return redirect(url_for(login))
-    return wrap
+
+#def login_required(test):
+#    @wraps(test)
+#    def wrap(*args, **kwargs):
+#        if 'logged_in' in session:
+#            return test(*args, **kwargs)
+#        else:
+#            flash('You need to log in first.')
+#            return redirect(url_for(login))
+#    return wrap
 
 
 @app.route('/', methods=['GET', 'POST'])
