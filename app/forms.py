813b701cc54c9364eb55b4d4b37932f374223643
Cruor99 <kliknes@gmail.com>
2014-01-31 22:52:22 +0100

    Users and login sessioning

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,17 @@
 __author__ = 'cruor'
+from flask.ext.wtf import Form
+from wtforms import TextField, BooleanField
+from wtforms.validators import Required
+
+
+class LoginForm(Form):
+    openid = TextField('openid', validators = [Required()])
+    remember_me = BooleanField('remember_me', default = False)
+
+
+class signup_form(Form):
+    username = TextField('Username', [validators.Required()])
+    password = PasswordField('Password', [validators.Required(), validators.EqualTo('confirm', message='Passwords must match')])
+    confirm = PasswordField('Confirm Password', [validators.Required()])
+    email = TextField('eMail', [validators.Required()])
+    accept_tos = BooleanField('I accept the TOS', [validators.Required])
--- app/views.py
+++ app/views.py
@@ -1,125 +1,80 @@
 __author__ = 'cruor'
-
-from flask import *
-from flask import request
-from functools import wraps
-from flask.ext.login import LoginManager
-import os
-
-
-app = Flask(__name__)
-app.config.from_object('config')
-app.secret_key = 'minecraft'
-login_manager = LoginManager()
+from flask import render_template, flash, redirect, session, url_for, request, g
+from flask.ext.login import login_user, logout_user, current_user, login_required
+from app import app, db, lm, oid
+from forms import LoginForm
+from models import User, ROLE_USER, ROLE_ADMIN
 
 
-@login_manager.user_loader
+@lm.user_loader
 def load_user(id):
     return User.query.get(int(id))
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.before_request
+def before_request():
+    g.user = current_user
+
+
+@oid.after_login
+def after_login(resp):
+    if resp.email is None or resp.email == "":
+        flash('Invalid login. Please try again.')
+        return redirect(url_for('login'))
+    user = User.query.filter_by(email = resp.email).first()
+    if user is None:
+        nickname = resp.nickname
+        if nickname is None or nickname == "":
+            nickname = resp.email.split('@')[0]
+        user = User(nickname = nickname, email = resp.email, role = ROLE_USER)
+        db.session.add(user)
+        db.session.commit()
+    remember_me = False
+    if 'remember_me' in session:
+        remember_me = session['remember_me']
+        session.pop('remember_me', None)
+    login_user(user, remember = remember_me)
+    return redirect(request.args.get('next') or url_for('index'))
+
+
+@app.route('/')
+@app.route('/index')
+@login_required
 def index():
-    return render_template('index.html')
+    user = g.user
+    posts = [
+        {
+            'author': { 'nickname': 'John' },
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': { 'nickname': 'Susan' },
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html',
+        title = 'Home',
+        user = user,
+        posts = posts)
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods = ['GET', 'POST'])
+@oid.loginhandler
 def login():
-    #nothing yet
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error ='Invalid login credentials'
-        else:
-            session['logged_in'] = True
-            return redirect(url_for('welcome'))
-    return render_template('login.html')
-
+    if g.user is not None and g.user.is_authenticated():
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        session['remember_me'] = form.remember_me.data
+        return oid.try_login(form.openid.data, ask_for = ['nickname', 'email'])
+    return render_template('login.html',
+        title = 'Sign In',
+        form = form,
+        providers = app.config['OPENID_PROVIDERS'])
 
 @app.route('/logout')
 def logout():
-    session.pop('logged_in', None)
-    flash("You were logged out.")
-    return redirect(url_for('login'))
+    logout_user()
+    return redirect(url_for('index'))
 
 
-
-def login_required(test):
-    @wraps(test)
-    def wrap(*args, **kwargs):
-        if 'logged_in' in session:
-            return test(*args, **kwargs)
-        else:
-            flash('You need to log in first.')
-            return redirect(url_for('login'))
-    return wrap
-
-
-@app.route('/welcome')
-@login_required
-def welcome():
-    return render_template('welcome.html')
-
-
-@app.route('/server', methods=['GET', 'POST'])
-@login_required
-def servermonitor():
-    error=None
-    #checks to initiate server script method
-    #NOT COMPLETE, ONLY TEST FOR NOW
-    serv = Server()
-
-    if request.method =='GET':
-        serverinfo =serv.serverRead()
-        return render_template('server.html', serverinfo=serverinfo)
-
-
-    if request.method =='POST':
-        serv.serverStart(mcServer)
-        return render_template('server.html')
-
-    else:
-        error='Did not INIT'
-    return render_template('server.html')
-
-class Server(object):
-    def __init__(self):
-        self.process = False
-        folder = "/home/cruor/mcServer"
-    def serverStart(user):
-        os.chdir("/home/cruor/"+user)
-        process =subprocess.Popen("./start.sh", close_fds=True)
-
-    def serverRead(self):
-        os.chdir("/home/cruor/"+user)
-       # self.process.stdout.seek(2)
-
-    def serverStop(self):
-        if self.process:
-            self.serverCom("stop")
-            self.process = False
-            return True
-        return False
-
-    def serverCom(self, text):
-        if self.process:
-            self.process.stdout.seek(2)
-            self.process.stdin.write("%s\n"%text)
-            self.process.stdin.flush()
-            self.process.stdout.flush()
-            return (str(self.process.stdout.readline()), True)
-        return ("", False)
-
-    def serverPlayers(selfself):
-        if self.process:
-            self.serverCom("list")
-            x = self.serverCom(" ")[0].split(":")[3].replace("\n","").replace(" ","")
-            if x =="":
-                x = 0
-            else:
-                x = len(x.split(","))
-            return (x, self.max)
-        return (0, self.max)
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
